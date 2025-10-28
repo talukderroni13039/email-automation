6fec249a8dbd5053c66e36678c8a562ab512ade6
Talukder Roni <talukder.roni.ict@gmail.com>
2025-10-28 18:25:06 +0600

    added attachments

--- send-email.ps1
+++ send-email.ps1
@@ -68,8 +68,10 @@ $body = @"
 
 # Attachments
 $attachments = @(
-    "C:\Users\LENOVO\Desktop\India-visa-Daily-Mail\passport_copy.pdf"
-   
+    "./attachments/BGDDV925AE25MD930620.pdf.pdf",
+   "./attachments/passport_copy.pdf",
+   "./attachments/Gmail - Request for Appointment Date.pdf",
+   "./attachments/AdmissionLetter.de.en (2).pdf",
 )
 
 # Send email  -Bcc $bcc
@@ -77,3 +79,4 @@ Send-MailMessage -From $from -To $to -Subject $subject -Body $body `
 -SmtpServer $smtpServer -Port $smtpPort -UseSsl:$useSsl -Credential $credential -Attachments $attachments -BodyAsHtml:$true
 
 Write-Host "Email sent successfully!"
+
